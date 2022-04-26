ec72b62eb4f68e0330a6e65f11831f042fbccf86
Alexandre Pineau <alecpineau@gmail.com>
2022-04-26 00:01:22 -0400

    added info box for displaying planet data

---
+++ index.js
@@ -0,0 +1,37 @@
+// Planets ordered by ascending distance from the Sun
+
+document.getElementById("mercury-img").addEventListener("mouseover", function () {
+    document.getElementById("planet-data").innerHTML = mercuryData;
+});
+
+document.getElementById("venus-img").addEventListener("mouseover", function () {
+    document.getElementById("planet-data").innerHTML = venusData;
+});
+
+document.getElementById("earth-img").addEventListener("mouseover", function () {
+    document.getElementById("planet-data").innerHTML = earthData;
+});
+
+document.getElementById("mars-img").addEventListener("mouseover", function () {
+    document.getElementById("planet-data").innerHTML = marsData;
+});
+
+document.getElementById("jupiter-img").addEventListener("mouseover", function () {
+    document.getElementById("planet-data").innerHTML = jupiterData;
+});
+
+document.getElementById("saturn-img").addEventListener("mouseover", function () {
+    document.getElementById("planet-data").innerHTML = saturnData;
+});
+
+document.getElementById("uranus-img").addEventListener("mouseover", function () {
+    document.getElementById("planet-data").innerHTML = uranusData;
+});
+
+document.getElementById("neptune-img").addEventListener("mouseover", function () {
+    document.getElementById("planet-data").innerHTML = neptuneData;
+});
+
+document.getElementById("pluto-img").addEventListener("mouseover", function () {
+    document.getElementById("planet-data").innerHTML = plutoData;
+});
